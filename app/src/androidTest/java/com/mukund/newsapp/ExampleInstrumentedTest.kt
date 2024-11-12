7807bd33acfae1813543165510f7537e8f5e9867
Mukund Rakholiya <155869925+mukund-rakholiya@users.noreply.github.com>
2024-11-12 09:37:43 +0530

    Update ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/mukund/newsapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/mukund/newsapp/ExampleInstrumentedTest.kt
@@ -2,7 +2,7 @@ package com.mukund.newsapp
   
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+ 
 import org.junit.Test
 import org.junit.runner.RunWith
 
